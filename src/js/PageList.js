6f6c436c094f80f2adbddeb2daf2816bb640af32
Nabil Labrazi <na.labrazi@gmail.com>
2021-01-28 18:02:17 +0100

    homepage

--- src/js/PageList.js
+++ src/js/PageList.js
@@ -22,14 +22,18 @@ const PageList = (argument = "") => {
         console.log("pagelist response :", response);
         response.results.forEach((game) => {
           games += `
-          <div class="col">
+          <div class="col" id="testId">
             <a href = "#pagedetail/${game.id}">
-              <div class="cardEachGame">
+              <div class="cardEachGame" id="cardFront">
               <img src="${game.background_image}" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">${game.name}</h5>
                   <p class="card-platform">${pictPlatforms(game.parent_platforms)}</p>
                 </div>
+              </div>
+              <div class="col" id="toShow" style="display:none;">
+                <p>${game.released}</p>
+                <p>${game.rating}</p>
               </div>
             </a>
           </div>
@@ -37,11 +41,12 @@ const PageList = (argument = "") => {
         });
         document.querySelector(".games").innerHTML = games;
 
-
+        //showmore
         const showMore = () => {
-          let button = document.getElementById("button");
+          //console.log("FUNCTION : showMore");
+          let button = document.getElementById("divButtonMore");
+          button.innerHTML = `<button id="button" class="btn btn-primary">ShowMore</button>`
           const newFetch = () => {
-            console.log("coucou");
             button.removeEventListener("click", newFetch);
             fetchList(response.next)
             console.log("response next:", response.next);
@@ -49,9 +54,48 @@ const PageList = (argument = "") => {
           button.addEventListener("click", newFetch);
         };
         showMore();
+        //showmore
+
+
+        //hover
+        const hoverFunction = () => {
+
+          //console.log("FUNCTION : hover");
+          const groupCards = document.querySelectorAll("#testId");
+          //console.log("each cards " + groupCards);
+
+          groupCards.forEach((card) => {
+            card.addEventListener("mouseover", () => {
+              card.childNodes[1].childNodes[1].style.display="none";
+              // console.log(card.childNodes[1].childNodes[1]);
+              // console.log(card.childNodes[1].childNodes[3]);
+              card.childNodes[1].childNodes[3].style.display="block";
+              //this.innerHTML = ``;
+              //this.innerHTML += ``
+            })
+          })
+
+          groupCards.forEach((card) => {
+            card.addEventListener("mouseleave", () => {
+              card.childNodes[1].childNodes[1].style.display="block";
+              card.childNodes[1].childNodes[3].style.display="none";
+              //this.innerHTML = ``;
+            })
+          })
+
+        };
+        hoverFunction();
+        //hover
+
+
+      //filter
+      //filter
 
 
       });
+
+      //fetch
+
     };
     fetchList("https://api.rawg.io/api/games", cleanedArgument, pageSize);
   };
@@ -61,7 +105,9 @@ const PageList = (argument = "") => {
     console.log("render PageList");
     pageDetailContent.innerHTML = ``
     pageContent.innerHTML = `
+    <div class="row row-cols-3 games">
     <h2>...loading<h2>
+    </div>
     `;
     preparePage();
   };
--- src/js/toolsList.js
+++ src/js/toolsList.js
@@ -7,16 +7,16 @@ const pictPlatforms = (platforms) => {
   platforms.forEach(platform => {
     switch(platform.platform.name) {
       case "PC":
-      platformsAvailable += `<img src="./src/images/windows.svg" alt="icon-pc">`;
+      platformsAvailable += `<img src="./src/images/windows.svg" alt="icon-pc" class="gamePC">`;
       break;
       case "Xbox":
-      platformsAvailable += `<img src="./src/images/xbox.svg" alt="icon-pc">`;
+      platformsAvailable += `<img src="./src/images/xbox.svg" alt="icon-xbox" class="gameXBOX">`;
       break;
       case "Playstation":
-      platformsAvailable += `<img src="./src/images/ps4.svg" alt="icon-pc">`;
+      platformsAvailable += `<img src="./src/images/ps4.svg" alt="icon-pc" class="gamePS">`;
       break;
       case "Nintendo":
-      platformsAvailable += `<img src="./src/images/switch.svg" alt="icon-pc">`;
+      platformsAvailable += `<img src="./src/images/switch.svg" alt="icon-pc" class="gameSwitch">`;
       break;
           default: //leave blank
           break;
@@ -28,11 +28,11 @@ const pictPlatforms = (platforms) => {
 
 
 const hoverCards = () => {
-  let hoverCard = document.getElementsByClassName("cardEachGame").onmouseover = function() {
-    console.log("pourt");
-    console.log(hoverCard);
-    this.style.backgroundColor = "blue";
-  };
+  // let hoverCard = document.getElementsByClassName("cardEachGame").onmouseover = function() {
+  //   console.log("pourt");
+  //   console.log(hoverCard);
+  //   this.style.backgroundColor = "blue";
+  // };
 };
 
 
