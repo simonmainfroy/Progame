5bfee3139872c508c362552f34580834f3bd50fe
Nabil Labrazi <na.labrazi@gmail.com>
2021-01-28 09:56:59 +0100

    final version 2.0

--- src/js/PageList.js
+++ src/js/PageList.js
@@ -1,83 +1,73 @@
+import { pictPlatforms, OldshowMore, hoverCards } from './toolsList';
+
+
+
 const PageList = (argument = "") => {
+
   const preparePage = () => {
     var cleanedArgument = argument.replace(/\s+/g, "-");
     let games = "";
+    const pageSize = "?page_size=9";
 
-    const pictPlatforms = (platforms) => {
-      let platformsAvailable = "";
-      platforms.forEach(platform => {
-        switch(platform.platform.name) {
-          case "PC":
-          platformsAvailable += `<img src="./src/images/windows.svg" alt="icon-pc">`;
-          break;
-          case "Xbox":
-          platformsAvailable += `<img src="./src/images/xbox.svg" alt="icon-pc">`;
-          break;
-          case "Playstation":
-          platformsAvailable += `<img src="./src/images/ps4.svg" alt="icon-pc">`;
-          break;
-          case "Nintendo":
-          platformsAvailable += `<img src="./src/images/switch.svg" alt="icon-pc">`;
-          break;
-          default: //leave blank
-          break;
-        }
-      });
-      return platformsAvailable;
-    }
-
-
-    const fetchList = (url, argument) => {
+    const fetchList = (url, argument, pageSize) => {
       let finalURL = url;
       if (argument) {
         finalURL = url + "?search=" + argument;
-      }
-
+      } else {
+        finalURL = url + pageSize;
+      };
       fetch(`${finalURL}`)
       .then((response) => response.json())
       .then((response) => {
-        console.log("pagelist reponse :", response);
+        console.log("pagelist response :", response);
         response.results.forEach((game) => {
           games += `
           <div class="col">
             <a href = "#pagedetail/${game.id}">
               <div class="cardEachGame">
-                <img src="${game.background_image}" class="card-img-top" alt="...">
+              <img src="${game.background_image}" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">${game.name}</h5>
-                  <p class="card-platform">
-                  ${pictPlatforms(game.parent_platforms)}
-                  </p>
+                  <p class="card-platform">${pictPlatforms(game.parent_platforms)}</p>
                 </div>
               </div>
             </a>
           </div>
           `;
-
-
-
         });
         document.querySelector(".games").innerHTML = games;
-        // document.querySelector(".page-list .games").innerHTML = games; 🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴
+
+
+        const showMore = () => {
+          let button = document.getElementById("button");
+          const newFetch = () => {
+            console.log("coucou");
+            button.removeEventListener("click", newFetch);
+            fetchList(response.next)
+            console.log("response next:", response.next);
+          };
+          button.addEventListener("click", newFetch);
+        };
+        showMore();
+
+
       });
     };
-
-    fetchList("https://api.rawg.io/api/games", cleanedArgument);
+    fetchList("https://api.rawg.io/api/games", cleanedArgument, pageSize);
   };
+
 
   const render = () => {
     console.log("render PageList");
     pageDetailContent.innerHTML = ``
     pageContent.innerHTML = `
-      ...loading
+    <h2>...loading<h2>
     `;
-    // <section class="page-list">
-    // </section>                     🔴🔴🔴🔴🔴🔴🔴
-
     preparePage();
   };
-
   render();
 };
+
+
 
 export { PageList };
